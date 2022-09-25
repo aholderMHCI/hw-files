4e2051d0c839d7fb5d7cf68a849d8ee5b3d6e1aa
aholderMHCI <112137355+aholderMHCI@users.noreply.github.com>
2022-09-24 21:53:36 -0400

    Cleaned up the code a bit

--- hw-3-solution/js/app.js
+++ hw-3-solution/js/app.js
@@ -12,54 +12,52 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptionElement/Option
 // And lastly, rubber ducky debugging with my wife, Erin
 
-let allGlazing = [ 
-    {
-        text: 'keep original - $2.49/each',
-        price: 0
-    },
-    {
-        text: 'sugar milk - $2.49/each',
-        price: 0
-    },
-    {
-        text: 'vanilla milk - $2.99/each',
-        price: .5
-    },
-    {
-        text: 'doubleChocolate $3.99/each',
-        price: 1.5
-    }
+let allGlazing = [{
+    text: 'keep original - $2.49/each',
+    price: 0
+},
+{
+    text: 'sugar milk - $2.49/each',
+    price: 0
+},
+{
+    text: 'vanilla milk - $2.99/each',
+    price: .5
+},
+{
+    text: 'doubleChocolate $3.99/each',
+    price: 1.5
+}
 ]
 
-let allSizing = [ 
-    {
-        text: '1',
-        quantity: 1
-    },
-    {
-        text: '3',
-        quantity: 3
-    },
-    {
-        text: '6 (pay for 5, get 6th free)',
-        quantity: 5
-    },
-    {
-        text: '12 (cheaper by the dozen!)',
-        quantity: 10
-    }
+let allSizing = [{
+    text: '1',
+    quantity: 1
+},
+{
+    text: '3',
+    quantity: 3
+},
+{
+    text: '6 (pay for 5, get 6th free)',
+    quantity: 5
+},
+{
+    text: '12 (cheaper by the dozen!)',
+    quantity: 10
+}
 ]
 
 let select = document.getElementById("glazing");
 
-for(index in allGlazing) {
-    select.options[select.options.length] = new Option(allGlazing[index].text, allGlazing[index].price);
+for (index in allGlazing) {
+select.options[select.options.length] = new Option(allGlazing[index].text, allGlazing[index].price);
 }
 
 let packSize = document.getElementById("pack-size");
 
-for(index in allSizing) {
-    packSize.options[packSize.options.length] = new Option(allSizing[index].text, allSizing[index].quantity);
+for (index in allSizing) {
+packSize.options[packSize.options.length] = new Option(allSizing[index].text, allSizing[index].quantity);
 }
 
 let basePrice = 2.49;
@@ -73,18 +71,13 @@ let quantityChange = 1;
 let glazePriceOption = document.getElementById("total-price");
 
 function glazingChange(element) {
-    priceChange = Number(element.value) + Number(basePrice);
-    totalPrice = (priceChange * quantityChange).toFixed(2);
-    glazePriceOption.innerHTML = "$" + totalPrice;
+priceChange = Number(element.value) + Number(basePrice);
+totalPrice = (priceChange * quantityChange).toFixed(2);
+glazePriceOption.innerHTML = "$" + totalPrice;
 }
 
 function sizingChange(element) {
-    quantityChange = Number(element.value);
-    totalPrice = (priceChange * quantityChange).toFixed(2);
-    glazePriceOption.innerHTML = "$" + totalPrice;
+quantityChange = Number(element.value);
+totalPrice = (priceChange * quantityChange).toFixed(2);
+glazePriceOption.innerHTML = "$" + totalPrice;
 }
-
-
-
-
-
