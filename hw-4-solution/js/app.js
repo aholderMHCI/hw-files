d65878e4488bada073c5ccdbc52f5b84ffb53c64
aholderMHCI <112137355+aholderMHCI@users.noreply.github.com>
2022-10-01 19:45:22 -0400

    HW 4 solution - complete

--- hw-4-solution/js/app.js
+++ hw-4-solution/js/app.js
@@ -1,31 +1,18 @@
 
-// Resources
-// I found the following github example useful in thinking through this problem:
-// https://github.com/interactive-structures/teach-pui/blob/main/in-lab-examples/puinote-lab04/select-example/app.js
-// Refresher on js conditionals:
-// https://www.w3schools.com/js/js_if_else.asp
-// This youtube video was a similar problem and was helpful:
-// https://www.youtube.com/watch?v=YeFzkC2awTM
-// useful doc for innerText:
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText
-// helpful for Option in js:
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptionElement/Option
-// And lastly, rubber ducky debugging with my wife, Erin
-
 let allGlazing = [{
-    text: 'keep original - $2.49/each',
+    text: 'keep original',
     price: 0
 },
 {
-    text: 'sugar milk - $2.49/each',
+    text: 'sugar milk',
     price: 0
 },
 {
-    text: 'vanilla milk - $2.99/each',
+    text: 'vanilla milk',
     price: .5
 },
 {
-    text: 'doubleChocolate $3.99/each',
+    text: 'double chocolate',
     price: 1.5
 }
 ]
@@ -39,11 +26,11 @@ let allSizing = [{
     quantity: 3
 },
 {
-    text: '6 (pay for 5, get 6th free)',
+    text: '6',
     quantity: 5
 },
 {
-    text: '12 (cheaper by the dozen!)',
+    text: '12',
     quantity: 10
 }
 ]
@@ -60,24 +47,75 @@ for (index in allSizing) {
 packSize.options[packSize.options.length] = new Option(allSizing[index].text, allSizing[index].quantity);
 }
 
-let basePrice = 2.49;
+const queryString = window.location.search;
 
-let priceChange = 2.49;
+const params = new URLSearchParams(queryString);
 
-let totalPrice = 2.49;
+const chosenRoll = params.get('roll')
+
+let cost = rolls[`${chosenRoll}`].basePrice;
+
+let priceChange = cost;
+
+let totalPrice;
 
 let quantityChange = 1;
 
-let glazePriceOption = document.getElementById("total-price");
+let priceOption = document.getElementById("total-price");
+
+priceOption.innerHTML = "$" + cost;
+
+
 
 function glazingChange(element) {
-priceChange = Number(element.value) + Number(basePrice);
-totalPrice = (priceChange * quantityChange).toFixed(2);
-glazePriceOption.innerHTML = "$" + totalPrice;
+    priceChange = Number(element.value) + Number(cost);
+    totalPrice = (priceChange * quantityChange).toFixed(2);
+    priceOption.innerHTML = "$" + totalPrice;
 }
 
 function sizingChange(element) {
-quantityChange = Number(element.value);
-totalPrice = (priceChange * quantityChange).toFixed(2);
-glazePriceOption.innerHTML = "$" + totalPrice;
+    quantityChange = Number(element.value);
+    totalPrice = (priceChange * quantityChange).toFixed(2);
+    priceOption.innerHTML = "$" + totalPrice;
 }
+
+
+const headerElement = document.querySelector('#roll-header-text');
+
+if (chosenRoll === 'Doublechocolate') {
+    headerElement.innerText = 'Double chocolate cinnamon roll';
+} else {
+    headerElement.innerText = chosenRoll + ' cinnamon roll';
+}
+
+const rollImage = document.getElementById('#roll-img');
+
+let imgUrl = rolls[`${chosenRoll}`].imageFile;
+
+rollImage.src = './assets/' + imgUrl; 
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+const cart = [];
+let myRoll;
+
+function addToCart(myRoll) {
+    myRoll = new Roll();
+    myRoll.type = chosenRoll;
+    // I was confused by how to access the contents of the dropdown because whatever I was doing kept providing 
+    // the 'price' from the allGlazing array. This answer from stack overflow helped.
+    // https://stackoverflow.com/questions/3301688/how-do-you-get-the-currently-selected-option-in-a-select-via-javascript
+    myRoll.glazing = select.options[select.selectedIndex].text;
+    myRoll.size = packSize.options[packSize.selectedIndex].text;
+    myRoll.basePrice = cost;
+    cart.push(myRoll);
+    console.log(cart);
+}
+
